ec2832340a78d8cbc2ee63d26492ee9d5fb4dab4
Prathap kumar <prathapkumar3967@gmail.com>
2026-03-02 23:38:39 +0530

    connected with postgres database

--- todo_backend/settings.py
+++ todo_backend/settings.py
@@ -25,18 +25,20 @@ SECRET_KEY = 'django-insecure-2ki!jd*bgqg7e9*j)fk=(q#1-3$ah58pe9a_5r$)=-$y==1z@f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'new',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +68,16 @@ TEMPLATES = [
     },
 ]
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+AUTH_USER_MODEL = "new.User"
+
+
 WSGI_APPLICATION = 'todo_backend.wsgi.application'
 
 
@@ -74,10 +86,15 @@ WSGI_APPLICATION = 'todo_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo_db',  # The name you gave the DB in pgAdmin
+        'USER': 'postgres',            # Default superuser
+        'PASSWORD': 'prathap',   # The password you set during installation
+        'HOST': 'localhost',           # Or '127.0.0.1'
+        'PORT': '5432',                # Default PostgreSQL port
     }
 }
+
 
 
 # Password validation
